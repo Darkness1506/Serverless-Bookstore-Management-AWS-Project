18c6e4fcba95bddb65f3d492a77fe04f7401b13d
Om Shinde <shindeoms15@gmail.com>
2026-02-27 12:41:32 +0530

    created apiResponse.js

---
+++ src/model/ApiRespnse.js
@@ -0,0 +1,32 @@
+// src/model/ApiResponse.js
+
+const defaultHeaders = {
+    "Content-type" : "application/json",
+    "Access-Control-Allow-Origin" : "*",
+    "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS"
+};
+
+const buildResponse = (statusCode, payload) => {
+    return {
+        statusCode,
+        headers : defaultHeaders,
+        body : JSON.stringify(payload ?? {}),
+        isBase64Encoded : false
+    };
+};
+
+const success = (statusCode = 200, body = null, message = "Success") => {
+    return buildResponse (statusCode, {
+        success : true,
+        body,
+        message
+    });
+};
+
+const error = (statusCode = 500, body = null, message = "Internal Server Error") => {
+    return buildResponse (statusCode, {
+        success : true,
+        body,
+        message
+    });
+};
